adec2eedf080503d0fbdad03345f1f7ec37f25be
albertoi <a.iglesiasd.2018@alumnos.urjc.es>
2022-02-16 14:17:59 +0100

    cambios en js y html

--- P1/tienda.js
+++ P1/tienda.js
@@ -9,12 +9,37 @@ const PUERTO = 9090;
 //-- Función de retrollamada de petición recibida
 //-- Cada vez que un cliente realiza una petición
 //-- Se llama a esta función
-function atender(req, res) {
-    //-- Indicamos que se ha recibido una petición
-    console.log(" " +"Petición recibida!" + " ");
-    //-- pero no enviamos respusta (todavía)
-}
-//tipos de extensiones
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    //-- Valores de la respuesta por defecto
+    let code = 200;
+    let code_msg = "OK";
+    let page = pagina_main;
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //-- Cualquier recurso que no sea la página principal
+    //-- genera un error
+    if (url.pathname != '/') {
+        code = 404;
+        code_msg = "Not Found";
+        page = pagina_error;
+    }
+
+    //-- Generar la respusta en función de las variables
+    //-- code, code_msg y page
+    res.statusCode = code;
+    res.statusMessage = code_msg;
+    res.setHeader('Content-Type','text/html');
+    res.write(page);
+    res.end();
+});
+
+//Definimos extensiones
 const Extensions = {
     "html" : "text/html",
     "png"  : "image/png",
@@ -22,8 +47,7 @@ const Extensions = {
     "jpg"  : "image/jpg",
     "JPG"  : "image/jpg"
   };
-//-- Activar la función de retrollamada del servidor
-server.on('request', atender);
+
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
 server.listen(PUERTO);
