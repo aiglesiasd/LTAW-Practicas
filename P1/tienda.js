47a1e84998a3637e077faae6d455536e3b914aba
albertoi <a.iglesiasd.2018@alumnos.urjc.es>
2022-04-19 12:45:44 +0200

    ya funciona

--- P1/tienda.js
+++ P1/tienda.js
@@ -1,53 +1,69 @@
-//Importamos modulos http y fs 
+// PRACTICA 1: Alberto Iglesias
+
+//-- Importar los siguientes modulos 
 const http = require('http');
 const fs = require('fs');
-
-//-- Crear el servidor
-const server = http.createServer();
 //-- Definir el puerto a utilizar 9090 como dicen las especificaciones
 const PUERTO = 9090;
+
+//-- Mensaje de arranque
+console.log("Arrancando servidor...");
 //-- Función de retrollamada de petición recibida
 //-- Cada vez que un cliente realiza una petición
 //-- Se llama a esta función
-const server = http.createServer((req, res)=>{
+const server = http.createServer(function (req, res) {
+
     console.log("Petición recibida!");
 
-    //-- Valores de la respuesta por defecto
-    let code = 200;
-    let code_msg = "OK";
-    let page = pagina_main;
-
-    //-- Analizar el recurso
-    //-- Construir el objeto url con la url de la solicitud
-    const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(url.pathname);
-
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
+    //-- Crear el objeto URL del mensaje de solitud (req)
+    //-- y coger el recurso (url)
+    let myURL = new URL(req.url, 'http://' + req.headers['host']);
+    console.log("\nSe ha solicitado el recurso: " + myURL.pathname);
+    //-- Definir la variable fichero
+    let FICHERO = "";
+    if (myURL.pathname == "/"){
+      FICHERO += "tienda.html";  //-- Abrir home
+    }else{
+      FICHERO = myURL.pathname;  
     }
 
-    //-- Generar la respusta en función de las variables
-    //-- code, code_msg y page
-    res.statusCode = code;
-    res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
-});
+    //-- Ruta asignada
+    console.log('Fichero a devolver: ' + FICHERO);
 
-//Definimos extensiones
-const Extensions = {
-    "html" : "text/html",
-    "png"  : "image/png",
-    "css"  : "text/css",
-    "jpg"  : "image/jpg",
-    "JPG"  : "image/jpg"
-  };
-
+    let ext = FICHERO.split(".")[1];
+    FICHERO = "." + FICHERO;
+    
+    //Extensions de extensiones
+    const Extensions = {
+        "html" : "text/html",
+        "jpg"  : "image/jpg",
+        "css"  : "text/css",
+        "png"  : "image/png",
+        "JPG"  : "image/jpg",
+        "ico"  : "image/x-icon"
+    };
+    let code = 200;
+    let code_msg = "OK";
+    //-- Asignar que tipo de mime leer
+    let mime = Extensions[ext];
+    console.log("Tipo solicitado: " + mime);
+    
+    fs.readFile(FICHERO, function(err, data){
+        if ((err) || (FICHERO == 'error.html')){
+            res.statusCode = 404;
+            res.statusMessage = "Not Found";
+            res.setHeader('Content-Type',mime);
+            console.log("Error");
+        }else{
+        console.log("Lectura completada, 200 OK")
+        res.setHeader('Content-Type', mime);
+        res.statusCode = code; 
+        res.statusMessage = code_msg;
+        } 
+        res.write(data); 
+        res.end(); //Terminar y enviar
+    });
+  });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
 server.listen(PUERTO);
