f51869966865670bbafed45a4402e4cf79b15790
albertoi <a.iglesiasd.2018@alumnos.urjc.es>
2022-05-08 18:19:15 +0200

    server p3 casi terminado

--- P3/chat/server.js
+++ P3/chat/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const colors = require('colors');
 const fs = require('fs');
 const PUERTO = 9090;
-
+cliente = 0;
 //-- Crear una nueva aplciacion web
 const app = express();
 
@@ -32,21 +32,43 @@ app.use(express.static('public'));
 //-- Evento: Nueva conexion recibida
 io.on('connect', (socket) => {
   
+  cliente += 1;
   console.log('** NUEVA CONEXIÓN **'.yellow);
-
+  io.send('Usuario conectado');
   //-- Evento de desconexión
   socket.on('disconnect', function(){
     console.log('** CONEXIÓN TERMINADA **'.yellow);
+    cliente -= 1;
+    io.send('Usuario se ha desconectado');
   });  
 
-  //-- Mensaje recibido: Reenviarlo a todos los clientes conectados
-  socket.on("message", (msg)=> {
-    console.log("Mensaje Recibido!: " + msg.green);
+socket.on("message", (msg)=> {  
+    console.log("Mensaje Recibido!: " + msg.green); 
+        if (msg[0] == "/") {
+            switch (msg) {
+                case '/hello':
+                    socket.send('Hola!');
+                    break;
+                case '/list':
+                    socket.send('Usuarios en linea: ' + cliente);
+                    break;
+                case '/help':
+                    socket.send('Comandos: <br> /help: ayuda con todos los comandos <br> /list: Lista de usuarios en linea .<br> /hello: Servidor devuelve un "hola".<br> /date: fecha actual.');
+                    break;
+                case '/date':
+                    let fecha = new Date(Date.now());
+                    let fecha1 = "Fecha ->" + fecha;
+                    socket.send(fecha1);
+                    break;
+                default:
+                    break;
+            }
+    }else {
+        //-- Reenviarlo a todos los clientes conectados
+        io.send(msg);
+    }
 
-    //-- Reenviarlo a todos los clientes conectados
-    io.send(msg);
   });
-
 });
 
 //-- Lanzar el servidor HTTP
