a6b6e933a5067cd5b5750f25d4c0c8441de7d53c
albertoi <a.iglesiasd.2018@alumnos.urjc.es>
2022-05-21 20:18:14 +0200

    cambios index

--- P4/index.js
+++ P4/index.js
@@ -1,4 +1,5 @@
 const electron = require('electron');
+const qrcode = require('qrcode');
 
 console.log("Hola desde el proceso de la web...");
 
@@ -8,16 +9,28 @@ const display = document.getElementById("display");
 const info1 = document.getElementById("info1");
 const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
+const arq = document.getElementById("info4");
+const plataforma = document.getElementById("info5");
+const directorio = document.getElementById("info6");
 const print = document.getElementById("print");
-
-//-- Acceder a la API de node para obtener la info
-//-- Sólo es posible si nos han dado permisos desde
-//-- el proceso princpal
-info1.textContent = process.arch;
-info2.textContent = process.platform;
-info3.textContent = process.cwd();
+const usuarios = document.getElementById("users");
+const dir = document.getElementById("dir");
+const qr = document.getElementById("qr");
 
 
+arq.textContent = process.arch;
+plataforma.textContent = process.platform;
+directorio.textContent = process.cwd();
+
+info1.textContent = process.versions.node;
+info2.textContent = process.versions.chrome;
+info3.textContent = process.versions.electron;
+
+//-- Numero de usuarios
+electron.ipcRenderer.on('users', (event, message) => {
+    console.log("Recibido: " + message);
+    usuarios.textContent = message;
+});
 btn_test.onclick = () => {
     display.innerHTML += "TEST! ";
     console.log("Botón apretado!");
@@ -31,3 +44,4 @@ electron.ipcRenderer.on('print', (event, message) => {
     console.log("Recibido: " + message);
     print.textContent = message;
   });
+  
